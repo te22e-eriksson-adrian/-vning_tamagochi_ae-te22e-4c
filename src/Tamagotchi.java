c3a4f7a2f9d073efd65703f3b92ec6b324e3c579
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-10-02 11:11:43 +0200

    skapade alla attribut och metoder som jag vet kommer att behövas för att uppnå det önskade resultatet

--- src/Tamagotchi.java
+++ src/Tamagotchi.java
@@ -1,3 +1,34 @@
-public class Tamagotchi {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class Tamagotchi {
+    // Attributes
+    private int hunger;
+    private int boredome;
+    private boolean isAlive;
+    private Random generator = new Random();
+    public String name;
+    private ArrayList<Tamagotchi> myTamagotchi = new ArrayList<>();
+}
+// Methods
+public static void feed(){
+    //jj
+}
+public static void speak(){
+    //jj
+}
+public static void teach(String word){
+    //jj
+}
+public static void tick(){
+    //jj
+}
+public static void printStats(){
+    //jj
+}
+public static boolean getAlive(){
+    //jj
+}
+private static void reduceBoredome(){
+    //jj
 }
